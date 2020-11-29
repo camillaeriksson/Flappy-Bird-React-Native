781ea3efbf3af4399dab87ae3415667caff76ec3
Camilla Eriksson <ericsson.camilla@gmail.com>
2020-11-29 14:44:10 +0100

    Restarted project

--- App.js
+++ App.js
@@ -1,21 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Matter from 'matter-js';
+import { GameEngine } from 'react-native-game-engine';
+import Bird from './Bird'
+import Constants from './Constants';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.gameEngine = null
+    this.entities = this.setupWorld()
+  }
+
+  setupWorld = () => {
+    let engine = Matter.Engine.create({ enableSleeping: false })
+    let world = engine.world;
+
+    let bird = Matter.Bodies.rectangle(Constants.MAX_WIDTH / 4, Constants.MAX_HEIGHT /2, 50, 50);
+
+    Matter.World.add(world, [bird])
+
+    return {
+      physics: { engine: engine, world: world },
+      bird: { body: bird, size: [50, 50], color: 'red', renderer: Bird },
+      // floor: { body: floor, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall },
+      // ceiling: { body: ceiling, size: [Constants.MAX_WIDTH, 50], color: 'green', renderer: Wall }
+    }
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <GameEngine
+          ref={(ref) => {this.gameEngine = ref; }}
+          style={styles.gameContainer}
+          entities={this.entities} 
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
